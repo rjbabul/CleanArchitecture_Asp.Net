8d65b6fb5db7143bd47d0717f23874e4defa52ab
rjbabul <rjbabul420@gmail.com>
2023-09-10 14:08:38 +0600

    Stracture of Clean Architechture

---
+++ WebApi/Program.cs
@@ -0,0 +1,36 @@
+
+using Application;
+using Presentation;
+using Infrastracture;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddApplication()
+    .AddPresentation()
+    .AddInfrastracture();
+
+builder.Host.UseSerilog((context, configuration) => 
+    configuration.ReadFrom.Configuration(context.Configuration));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSerilogRequestLogging();
+app.UseHttpsRedirection();
+
+  
+app.Run();
+ 
